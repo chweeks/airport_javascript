4d2b7e367e5f169aa298ea96b3d263cd3d9f569d
chweeks <chrisharryweeks@gmail.com>
2015-09-29 13:13:24 +0100

    fixed stubbing issues

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,12 +1,22 @@
 describe("Airport", function() {
   var plane;
-  var plane2;
   var airport;
 
   beforeEach(function() {
-    plane = new Plane();
-    plane2 = new Plane();
+
     airport = new Airport();
+
+    plane = {
+      isFlying: function() {
+        true;
+      },
+      land: function() {
+        this.isFlying = false;
+      },
+      take_off: function(){
+        this.isFlying = true
+      }
+    };
     weather = new Weather();
   });
 
@@ -15,14 +25,29 @@ describe("Airport", function() {
   });
 
   it('can instruct a plane to take_off', function(){
+    spyOn(weather, 'isStormy').and.returnValue(false);
     airport.instruct_to_land(plane)
     airport.instruct_to_take_off(plane)
     expect(airport.hanger.length).toEqual(0)
   });
 
   it('can instruct a plane to land', function() {
+    spyOn(weather, 'isStormy').and.returnValue(false);
     airport.instruct_to_land(plane)
     expect(airport.hanger).toEqual([plane])
+  });
+
+  it('Plane is flying after instructed to take off', function() {
+    spyOn(weather, 'isStormy').and.returnValue(false);
+    airport.instruct_to_land(plane);
+    airport.instruct_to_take_off(plane);
+    expect(plane.isFlying).toBe(true);
+  });
+
+  it('Plane is landed after instructed to land', function() {
+    spyOn(weather, 'isStormy').and.returnValue(false);
+    airport.instruct_to_land(plane);
+    expect(plane.isFlying).toBe(false);
   });
 
   it("should have capacity when created", function() {
@@ -30,34 +55,25 @@ describe("Airport", function() {
   });
 
   it('Plane can\'t land if hanger is full', function() {
+    spyOn(weather, 'isStormy').and.returnValue(false);
     airport.capacity = 1
     airport.instruct_to_land(plane)
-    expect(function(){ airport.instruct_to_land(plane2); }).toThrow("Hanger is full");
+    expect(function(){ airport.instruct_to_land(plane); }).toThrow("Hanger is full");
   });
 
   it('Can\'t instruct plane to take off if not in hanger', function(){
+    spyOn(weather, 'isStormy').and.returnValue(false);
     expect(function(){airport.instruct_to_take_off(plane); }).toThrow("Plane not in hanger");
   });
 
   it('Can\'t take off in stormy weather', function(){
     airport.instruct_to_land(plane)
-    spyOn(weather, 'chance').and.returnValue(0.95);
+    spyOn(weather, 'isStormy').and.returnValue(true);
     expect(function(){airport.instruct_to_take_off(plane); }).toThrow("Cannot take off in stormy weather");
   });
 
   it('Can\'t land in stormy weather', function(){
-    spyOn(weather, 'chance').and.returnValue(0.95);
+    spyOn(weather, 'isStormy').and.returnValue(true);
     expect(function(){airport.instruct_to_land(plane); }).toThrow("Cannot land in stormy weather");
-  });
-
-  it('Plane is flying after instructed to take off', function() {
-    airport.instruct_to_land(plane);
-    airport.instruct_to_take_off(plane);
-    expect(plane.isFlying).toBe(true);
-  });
-
-  it('Plane is landed after instructed to land', function() {
-    airport.instruct_to_land(plane);
-    expect(plane.isFlying).toBe(false);
   });
 });
